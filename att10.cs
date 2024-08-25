1882eb9319c7d65e60c1496fad77c856c8a1d41a
Pedro Henrique Rodrigues Evangelista <99209592+Pedro125600@users.noreply.github.com>
2024-08-25 19:51:10 -0300

    Add files via upload

---
+++ att10.cs
@@ -0,0 +1,27 @@
+﻿namespace att10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("De um numero inteiro para converter para binario: ");
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{Binario(N)}");
+        }
+
+        static string Binario(int n)
+        {
+            if(n == 1 || n == 0)
+            {
+                return n.ToString();
+            }
+
+            else
+            {
+                return Binario(n / 2) + (n % 2).ToString(); 
+            }
+
+
+        }
+    }
+}
